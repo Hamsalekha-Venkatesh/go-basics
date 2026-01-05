4f468e52ca71fc9f30376904c34dfad6e6bd7d7b
Hamsalekha Venkatesh <lekha.hamsa93@gmail.com>
2026-01-05 10:08:49 -0800

    Learn concurrent programming -- understand how goroutines are run concurrenrtly on multiple OS threads

---
+++ advanced/ticket-system-concurrency.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+/**
+Ticket selling and buying
+*/
+
+var (
+	TOTAL_AVAILABLE_TICKETS = 100
+	CONSUMED_TICKETS        = 0
+	totalRequest            = 15
+	totalWorker             = 3
+)
+
+type TicketRequest struct {
+	personId   int
+	numTickets int
+}
+
+type Result struct {
+	personId int
+	cost     int
+	err      error
+}
+
+func ticketProcessor(requestChan <-chan TicketRequest, resultsChan chan<- Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for request := range requestChan {
+		//if request.numTickets+CONSUMED_TICKETS > TOTAL_AVAILABLE_TICKETS {
+		//	fmt.Println("Show is full, can't purchase. Aborting tx")
+		//	resultsChan <- Result{personId: -1, cost: -1, err: errors.New("Show is full, can't purchase. Aborting tx")}
+		//	continue
+		//}
+
+		CONSUMED_TICKETS += request.numTickets
+
+		finalCost := 20 * request.numTickets
+		fmt.Printf("Person %d Booked %d tickets. Total cost: %d \n ", request.personId, request.numTickets, finalCost)
+		resultsChan <- Result{personId: request.personId, cost: finalCost, err: nil}
+
+	}
+}
+
+func main() {
+	var waitGroups sync.WaitGroup
+
+	requestChan := make(chan TicketRequest, totalRequest)
+	resultsChan := make(chan Result, totalRequest)
+
+	waitGroups.Add(totalWorker)
+
+	for range totalWorker {
+		go ticketProcessor(requestChan, resultsChan, &waitGroups)
+	}
+
+	for i := 1; i <= totalRequest; i++ {
+		ticketRequest := TicketRequest{
+			personId:   i,
+			numTickets: rand.Intn(TOTAL_AVAILABLE_TICKETS / 5),
+		}
+		requestChan <- ticketRequest
+	}
+
+	close(requestChan)
+	waitGroups.Wait()
+
+	for result := range resultsChan {
+		if result.err != nil {
+			fmt.Println("person: ", result.personId, "cost: ", result.cost)
+		}
+	}
+}
